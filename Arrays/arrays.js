ce6fb733fd18e8f6b288321d10122d693305e458
Karakalchev Roman <rombl4_83@mail.ru>
2020-08-26 02:13:47 +0700

    Исправлены замечания по задачам

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,36 +1,34 @@
 (function() {
     var numbersArray = [1, 2, 3, 5, 6, 8, 4, 9, 11, 10, 7];
 
-    console.log("Отсортированный массив по убыванию: " + numbersArray.sort(descendingSort).toString());
+    console.log("Отсортированный массив по убыванию: " + numbersArray.sort(function(a, b) {
+        return b-a;
+    }).toString());
     console.log("Первые пять элементов массива: " + numbersArray.slice(0, 5).toString());
     console.log("Последние пять элементов массива: " + numbersArray.slice(-5).toString());
 
     console.log("Сумма четных элементов массива: " + numbersArray
         .filter(isEvenNumber)
-        .reduce(getSum, 0)
+        .reduce(function (a, b) {
+            return a + b;
+        }, 0)
         .toString());
 
-    numbersArray = setArray(100);
+    numbersArray = createArrayNumbers(100);
 
     console.log("Список квадратов четных чисел массива от 1 до 100:");
     console.log(numbersArray
         .filter(isEvenNumber)
-        .map(element => element * element)
+        .map(function(element) {
+            return element * element;
+        })
         .toString());
 
-    function descendingSort(a, b) {
-        return (a - b) * (-1);
-    }
-
     function isEvenNumber(num) {
-        return (num % 2 === 0);
+        return num % 2 === 0;
     }
 
-    function getSum(a, b) {
-        return a + b;
-    }
-
-    function setArray(n) {
+    function createArrayNumbers(n) {
         var array = [];
         for (var i = 1; i <= n; i++) {
             array.push(i);
