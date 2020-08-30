f4bcfc9371daa09095d8be66c6207daa8618677a
Karakalchev Roman <rombl4_83@mail.ru>
2020-08-31 01:44:09 +0700

    Исправлены замечания по задаче "Работа с массивами": 1. Нужно отформатировать код
    2. Сортировку лучше сделать отдельной командой.
    Т.е. не вставлять ее в код печати
    
    3. createArrayNumbers:
    - нужно перенести Array в конец имени.
    В английском нужно указывать сначала "чье", а потом "что"
    - не хватает пустой строки перед циклом
    
    4. Лучше не переприсваивать numbersArray, а сделать отдельную переменную
    
    5. В reduce лучше не называть аргументы a, b.
    Потому что у этих аргументов разный смысл

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,25 +1,27 @@
-(function() {
+(function () {
     var numbersArray = [1, 2, 3, 5, 6, 8, 4, 9, 11, 10, 7];
 
-    console.log("Отсортированный массив по убыванию: " + numbersArray.sort(function(a, b) {
-        return b-a;
-    }).toString());
+    numbersArray = numbersArray.sort(function (a, b) {
+        return b - a;
+    });
+
+    console.log("Отсортированный массив по убыванию: " + numbersArray.toString());
     console.log("Первые пять элементов массива: " + numbersArray.slice(0, 5).toString());
     console.log("Последние пять элементов массива: " + numbersArray.slice(-5).toString());
 
     console.log("Сумма четных элементов массива: " + numbersArray
         .filter(isEvenNumber)
-        .reduce(function (a, b) {
-            return a + b;
+        .reduce(function (evenNumbersSum, evenNumber) {
+            return evenNumbersSum + evenNumber;
         }, 0)
         .toString());
 
-    numbersArray = createArrayNumbers(100);
+    var array = createNumbersArray(100);
 
     console.log("Список квадратов четных чисел массива от 1 до 100:");
-    console.log(numbersArray
+    console.log(array
         .filter(isEvenNumber)
-        .map(function(element) {
+        .map(function (element) {
             return element * element;
         })
         .toString());
@@ -28,8 +30,9 @@
         return num % 2 === 0;
     }
 
-    function createArrayNumbers(n) {
+    function createNumbersArray(n) {
         var array = [];
+
         for (var i = 1; i <= n; i++) {
             array.push(i);
         }
