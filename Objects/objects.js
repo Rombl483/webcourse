edee003015dd4c5e83672a5bfb1be98ce7c08418
Karakalchev Roman <rombl4_83@mail.ru>
2020-09-04 19:47:57 +0700

    Исправлены замечания по задаче "Работа с объектами": 1. countriesDataWithCityMaxCount, getCountriesDataWithCityMaxCount, cityMaxCount - нужно поменять местами слова city и max. В английском нужно указывать сначала "чье", а потом "что". И city нужно сделать во множественном числе
    2. citiesData лучше назвать cities, аналогично для countriesData, cityData, countryData.
    Слово data неинформативно и не дает дополнительного смысла
    
    3. cityName, cityPopulation, countryName - из имен лучше убрать слова city и country, т.к. это и так поля объектов "город" и "страна"
    
    4. При вычислении cityMaxCount нужно использовать Math.max
    
    5. В getCountriesPopulation плохо подходит внешний reduce.
    В reduce должна быть чистая функция.
    Поэтому здесь лучше использовать forEach
    
    6. Можно не вызывать toString для чисел при конкатенации

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,29 +1,29 @@
 (function () {
-    var countriesData = [
+    var countries = [
         {
-            countryName: "Россия",
-            citiesData: [
-                {cityName: "Москва", cityPopulation: 12678079},
-                {cityName: "Санкт - Петербург", cityPopulation: 5398064},
-                {cityName: "Новосибирск", cityPopulation: 1625631},
-                {cityName: "Екатирнбург", cityPopulation: 1493749}
+            name: "Россия",
+            cities: [
+                {name: "Москва", population: 12678079},
+                {name: "Санкт - Петербург", population: 5398064},
+                {name: "Новосибирск", population: 1625631},
+                {name: "Екатирнбург", population: 1493749}
             ]
         },
         {
-            countryName: "Казахстан",
-            citiesData: [
-                {cityName: "Нур-Султан", cityPopulation: 1136008},
-                {cityName: "Алма-Ата", cityPopulation: 1916782},
-                {cityName: "Шымкент", cityPopulation: 1036144}
+            name: "Казахстан",
+            cities: [
+                {name: "Нур-Султан", population: 1136008},
+                {name: "Алма-Ата", population: 1916782},
+                {name: "Шымкент", population: 1036144}
             ]
         },
         {
-            countryName: "США",
-            citiesData: [
-                {cityName: "Лос-Анджелес", cityPopulation: 3928864},
-                {cityName: "Нью-Йорк", cityPopulation: 8244910},
-                {cityName: "Чикаго", cityPopulation: 2722389},
-                {cityName: "Хьюстон", cityPopulation: 2239558}
+            name: "США",
+            cities: [
+                {name: "Лос-Анджелес", population: 3928864},
+                {name: "Нью-Йорк", population: 8244910},
+                {name: "Чикаго", population: 2722389},
+                {name: "Хьюстон", population: 2239558}
             ]
         }
     ];
@@ -31,47 +31,48 @@
     console.log("----------------------------------------------------------------------------");
     console.log("--------------------- Информация по всем старнам справочника ---------------");
     console.log("----------------------------------------------------------------------------");
-    countriesData.forEach(printCountryData);
+    countries.forEach(printCountryData);
 
     console.log("----------------------------------------------------------------------------");
     console.log("------------------ Страны с максимальным количеством городов ---------------");
     console.log("----------------------------------------------------------------------------");
-    var countriesDataWithCityMaxCount = getCountriesDataWithCityMaxCount(countriesData);
-    countriesDataWithCityMaxCount.forEach(printCountryData);
+    var countriesWithMaxCitiesCount = getCountriesWithMaxCitiesCount(countries);
+    countriesWithMaxCitiesCount.forEach(printCountryData);
 
     console.log("----------------------------------------------------------------------------");
     console.log("------- Суммарная численность населения городов по каждой стране -----------");
     console.log("----------------------------------------------------------------------------");
-    var countriesPopulation = getCountriesPopulation(countriesData);
+    var countriesPopulation = getCountriesPopulation(countries);
 
-    for (var countryName in countriesPopulation) {
-        console.log("Страна: " + countryName + ";  общая численность населения: " + countriesPopulation[countryName].toString());
+    for (var country in countriesPopulation) {
+        console.log("Страна: " + country + ";  общая численность населения: " + countriesPopulation[country]);
     }
 
-    function printCountryData(countryData) {
-        console.log("Страна: " + countryData.countryName);
-        countryData.citiesData.forEach(function (cityData) {
-            console.log("   город: " + cityData.cityName + ";  численность населения: " + cityData.cityPopulation.toString());
+    function printCountryData(country) {
+        console.log("Страна: " + country.name);
+        country.cities.forEach(function (city) {
+            console.log("   город: " + city.name + ";  численность населения: " + city.population);
         });
     }
 
-    function getCountriesDataWithCityMaxCount(countriesData) {
-        var cityMaxCount = countriesData.reduce(function (cityMaxCount, countryData) {
-            return cityMaxCount < countryData.citiesData.length ? countryData.citiesData.length : cityMaxCount;
+    function getCountriesWithMaxCitiesCount(countries) {
+        var maxCitiesCount = countries.reduce(function (maxCitiesCount, country) {
+            return Math.max(maxCitiesCount, country.cities.length);
         }, 0);
 
-        return countriesData.filter(function (countryData) {
-            return countryData.citiesData.length === cityMaxCount;
+        return countries.filter(function (country) {
+            return country.cities.length === maxCitiesCount;
         });
     }
 
-    function getCountriesPopulation(countriesData) {
-        return countriesData.reduce(function (countriesPopulation, countryData) {
-            countriesPopulation[countryData.countryName] = countryData.citiesData.reduce(function (populationSum, cityData) {
-                return populationSum + cityData.cityPopulation;
+    function getCountriesPopulation(countries) {
+        var countriesPopulation = {};
+        countries.forEach(function (country) {
+            countriesPopulation[country.name] = country.cities.reduce(function (populationSum, city) {
+                return populationSum + city.population;
             }, 0);
+        });
 
-            return countriesPopulation;
-        }, {});
+        return countriesPopulation;
     }
 })();
